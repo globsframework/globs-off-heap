a0da33295a94aa4d860a344a7858f1c952e6b5b2
MarcGuiot <marc.guiot@free.fr>
2025-09-15 07:57:08 +0200

    Test with sub object.

--- src/test/java/org/globsframework/shared/mem/GlobContentTest.java
+++ src/test/java/org/globsframework/shared/mem/GlobContentTest.java
@@ -4,6 +4,7 @@ import org.globsframework.core.functional.FunctionalKeyBuilder;
 import org.globsframework.core.functional.FunctionalKeyBuilderFactory;
 import org.globsframework.core.model.Glob;
 import org.globsframework.core.model.MutableGlob;
+import org.globsframework.json.GSonUtils;
 import org.globsframework.shared.mem.impl.Dummy1Type;
 import org.globsframework.shared.mem.impl.Dummy2Type;
 import org.globsframework.shared.mem.impl.Dummy3Type;
@@ -27,14 +28,9 @@ public class GlobContentTest {
         final MutableGlob d22 = Dummy2Type.create("d22");
         d1.set(Dummy1Type.subObject, d21);
         d1.set(Dummy1Type.subObjectInline, d22);
-//        d22.set(Dummy2Type.subObjects, new MutableGlob[]{Dummy3Type.create("ext1"),Dummy3Type.create("ext2")});
-//        d22.set(Dummy2Type.subObjectsInline, new MutableGlob[]{Dummy3Type.create("inl1"),Dummy3Type.create("inl2")});
+        d22.set(Dummy2Type.subObjects, new MutableGlob[]{Dummy3Type.create("ext1"),Dummy3Type.create("ext2")});
+        d22.set(Dummy2Type.subObjectsInline, new MutableGlob[]{Dummy3Type.create("inl1"),Dummy3Type.create("inl2")});
         d2.set(Dummy1Type.subObjectInline, Dummy2Type.create("d23"));
-
-        final FunctionalKeyBuilder uniqueFunctionalKeyBuilder =
-                FunctionalKeyBuilderFactory.create(Dummy1Type.TYPE)
-                        .add(Dummy1Type.id)
-                        .create();
 
         OffHeapService offHeapService = OffHeapService.create(Dummy1Type.TYPE);
 
@@ -51,8 +47,51 @@ public class GlobContentTest {
         List<Glob> readData = new ArrayList<>();
         readHeapService.readAll(readData::add);
         Assertions.assertEquals(2, readData.size());
-        Assertions.assertEquals("""
-[{ "_kind":"Dummy1Type", "id":0, "subObjectInline":{ "_kind":"Dummy2Type", "name":"d22", "aligned":0}, "subObject":{ "_kind":"Dummy2Type", "name":"d21", "aligned":0}}, { "_kind":"Dummy1Type", "id":0, "subObjectInline":{ "_kind":"Dummy2Type", "name":"d23", "aligned":0}}]                """, readData.toString());
-
+        Assertions.assertEquals(GSonUtils.normalize(EXPECTED),
+                GSonUtils.normalize(GSonUtils.encode(readData.toArray(Glob[]::new), false)));
     }
+
+    public static String EXPECTED = """
+[
+  {
+    "id": 0,
+    "subObjectInline": {
+      "name": "d23",
+      "aligned": 0
+    }
+  },
+  {
+    "id": 0,
+    "subObjectInline": {
+      "name": "d22",
+      "subObjectsInline": [
+        {
+          "data": "inl1",
+          "unaligned": false
+        },
+        {
+          "data": "inl2",
+          "unaligned": false
+        }
+      ],
+      "subObjects": [
+        {
+          "data": "ext1",
+          "unaligned": false
+        },
+        {
+          "data": "ext2",
+          "unaligned": false
+        }
+      ],
+      "aligned": 0
+    },
+    "subObject": {
+      "name": "d21",
+      "aligned": 0
+    }
+  }
+]""";
+
+
 }
